5ee887b55c35bca783e4ddba015f46abb4759c44
isaccanedo <isaccanedo@gmail.com>
2021-01-28 12:21:52 -0300

    :palm_tree: Initial commit

---
+++ socketAppClient/Mahasiswa.java
@@ -0,0 +1,35 @@
+import java.io.Serializable;
+
+// ++=========================++
+// || @author Isac Canedo     ||
+// ++=========================++
+
+public class Mahasiswa implements Serializable{
+    String nim=null;
+    String nama=null;
+    String progdi=null;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getProgdi() {
+        return progdi;
+    }
+
+    public void setProgdi(String progdi) {
+        this.progdi = progdi;
+    }
+}
